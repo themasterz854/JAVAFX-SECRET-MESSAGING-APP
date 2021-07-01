7ae7af9022e7dea06aa6517fdcc7e6b9eb9ad1cf
themasterz854 <62380365+themasterz854@users.noreply.github.com>
2021-07-02 00:10:03 +0530

    basic file sending implemented

--- src/sample/ChatDialogController.java
+++ src/sample/ChatDialogController.java
@@ -3,23 +3,29 @@ package sample;
 
 import javafx.concurrent.Task;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 
+import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
+import java.io.*;
 
 import static sample.clientlistcontroller.cid;
 
 public class ChatDialogController {
+    @FXML
+    private Button send_file_button;
     @FXML
     private TextField message;
     @FXML
     private TextArea ta;
     @FXML
     private TextArea myta;
+    @FXML
+    private Button dirchoose;
     int id;
 
     public void transferdata(){
@@ -31,30 +37,48 @@ public class ChatDialogController {
     String[] data = new String[4];
     String[] queue= new String[10];
     int front,rear;
+    String FileName;
+    int FileSize;
+    byte[] ReceivedData;
+    File directory;
+    DirectoryChooser dc = new DirectoryChooser();
+    FileOutputStream fos;
+
+    public void direchooser()
+    {
+        directory = dc.showDialog(null);
+    }
     public synchronized void checkandwrite() throws IOException{
         front = rear = -1;
-        while(din.available()> 0)
-        {
-            s = din.readUTF();
-            data = s.split(" ");
-            if(Integer.parseInt(data[0]) == id) {
-                for(int i =1;i<data.length;i++)
-                ta.appendText(data[i]+" ");
-                ta.appendText("\n");
-            }
-            else
-            {
-                if(front == -1)
-                {
-                    front = 0;
-                }
-                queue[++rear] = s;
 
+        while(din.available()> 0) {
+            s = din.readUTF();
+            if (s.equals("file")) {
+                FileName = din.readUTF();
+                FileSize = Integer.parseInt(din.readUTF());
+                ReceivedData = new byte[FileSize];
+                din.readFully(ReceivedData);
+                fos = new FileOutputStream(directory.getAbsolutePath()+"\\"+FileName);
+                fos.write(ReceivedData, 0, FileSize);
+                fos.close();
+            } else {
+                data = s.split(" ");
+                if (Integer.parseInt(data[0]) == id) {
+                    for (int i = 1; i < data.length; i++)
+                        ta.appendText(data[i] + " ");
+                    ta.appendText("\n");
+                } else {
+                    if (front == -1) {
+                        front = 0;
+                    }
+                    queue[++rear] = s;
+
+                }
+
+                while (front != rear + 1 && front != -1 && rear != -1) {
+                    dout.writeUTF("others" + " " + queue[front++]);
+                }
             }
-        }
-        while(front != rear + 1 && front != -1 && rear != -1)
-        {
-            dout.writeUTF("others"+" "+queue[front++]);
         }
     }
     public void run_task(){
@@ -100,5 +124,14 @@ public class ChatDialogController {
         dout.flush();
         myta.appendText(message.getText() + "\n");
         message.clear();
+    }
+
+    public void start_file_window() throws IOException {
+        Stage file_chooser = new Stage();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("FileChooser.fxml"));
+        Parent root = loader.load();
+        Scene list_scene = new Scene(root);
+        file_chooser.setScene(list_scene);
+        file_chooser.show();
     }
 }
---
+++ src/sample/FileChooserController.java
@@ -0,0 +1,74 @@
+package sample;
+
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.stage.FileChooser;
+
+import java.io.*;
+import java.util.List;
+
+
+public class FileChooserController {
+    @FXML
+    private Button select_one;
+    @FXML
+    private Button select_multiple;
+    @FXML
+    private ListView send_list;
+
+    @FXML
+    private Button sendfiles;
+
+    public void selectafile(){
+
+        FileChooser fc = new FileChooser();
+        fc.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("All","*"),new FileChooser.ExtensionFilter("Images","*.png","*.jpg"));
+        File selected_File = fc.showOpenDialog(null);
+
+        if(selected_File != null)
+        {
+            send_list.getItems().add(selected_File.getAbsolutePath());
+        }
+        else
+            System.out.print("Not valid file");
+    }
+    public void selectmultiplefiles(){
+        int i;
+        FileChooser fc = new FileChooser();
+        fc.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("All","*"),new FileChooser.ExtensionFilter("Images","*.png","*.jpg"));
+        List<File> selected_Files = fc.showOpenMultipleDialog(null);
+
+        if(selected_Files != null)
+        {
+            for(i=0;i<selected_Files.size();i++)
+            send_list.getItems().add(selected_Files.get(i).getAbsolutePath());
+        }
+        else
+            System.out.print("Not valid file");
+    }
+
+    public void sendthefiles() throws IOException {
+        int i;
+        File file;
+        FileInputStream fis;
+        DataOutputStream dout = new DataOutputStream(IntroController.s.getOutputStream());
+        for(i=0;i<send_list.getItems().size();i++)
+        {
+            file = new File((String) send_list.getItems().get(i));
+            fis = new FileInputStream(file);
+            byte sendData[] = new byte[(int)file.length()];
+            fis.read(sendData);
+            dout.writeUTF("file");
+            dout.flush();
+            dout.writeUTF(file.getName());
+            dout.flush();
+            dout.writeUTF(Integer.toString(sendData.length));
+            dout.flush();
+            dout.write(sendData,0,sendData.length);
+            dout.flush();
+        }
+    }
+
+}
